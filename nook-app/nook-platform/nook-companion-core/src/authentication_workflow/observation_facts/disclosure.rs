812271d36b8ca8519e3a56eb4b7e78fa4275ee50
cypherkitty <bynull@meta-secret.org>
2026-09-08 04:27:32 -0700

    Add versioned authentication disclosure control policy (#1554)

---
+++ nook-app/nook-platform/nook-companion-core/src/authentication_workflow/observation_facts/disclosure.rs
@@ -0,0 +1,13 @@
+#![cfg_attr(dylint_lib = "nook_domain_api", deny(unowned_function))]
+#![cfg_attr(
+    dylint_lib = "nook_domain_api",
+    forbid(invalid_unowned_function_suppression)
+)]
+
+mod control;
+
+pub use control::{
+    AuthenticationDisclosureControlDecision, AuthenticationDisclosureObservationSchemaVersion,
+    CurrentAuthenticationDisclosureControlRequest,
+    VersionedAuthenticationDisclosureControlObservation,
+};
