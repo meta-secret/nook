9d04f21c964cdd5f79d01c4f46e7ac3f10931ed4
cypherkitty <109506107+cypherkitty@users.noreply.github.com>
2026-09-10 03:42:31 -0700

    fix(web): restore sentinel connection adapter

--- nook-app/nook-web/nook-vault-sentinel/src/main.ts
+++ nook-app/nook-web/nook-vault-sentinel/src/main.ts
@@ -1,6 +1,6 @@
-import { VaultApplication } from '$app-wasm'
-import { vaultApplicationEntrypoint } from '$vault-shared/main'
+import { VaultApplication } from '$app-wasm';
+import { vaultApplicationEntrypoint } from '$vault-shared/main';
 
-await vaultApplicationEntrypoint.start(VaultApplication.Sentinel)
+await vaultApplicationEntrypoint.start(VaultApplication.Sentinel);
 
-export default {}
+export default {};
