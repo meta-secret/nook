9982aeb2349e0465fd7925976d9b7482c846bc46
cypherkitty <109506107+cypherkitty@users.noreply.github.com>
2026-09-19 23:48:48 -0700

    Remove obsolete preflight parser state

--- preflight/src/main.rs
+++ preflight/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::env;
 use std::ffi::OsString;
 use std::io;
@@ -13,7 +12,6 @@ fn main() {
 
 struct PreflightCommand {
     command: String,
-    options: HashMap<String, OsString>,
 }
 impl PreflightCommand {
     fn run(arguments: impl IntoIterator<Item = OsString>) -> io::Result<()> {
@@ -22,43 +20,19 @@ impl PreflightCommand {
             .next()
             .and_then(|value| value.into_string().ok())
             .ok_or_else(|| PreflightCommand::usage("expected a command"))?;
-        let options = PreflightCommand::parse_options(arguments)?;
+        if arguments.into_iter().next().is_some() {
+            return Err(PreflightCommand::usage(
+                "additional arguments are not supported",
+            ));
+        }
 
-        Self { command, options }.execute()
+        Self { command }.execute()
     }
     fn execute(self) -> io::Result<()> {
-        let Self { command, .. } = self;
+        let Self { command } = self;
         Err(PreflightCommand::usage(&format!(
             "unknown command {command}"
         )))
-    }
-}
-
-impl PreflightCommand {
-    fn parse_options(
-        arguments: impl IntoIterator<Item = OsString>,
-    ) -> io::Result<HashMap<String, OsString>> {
-        let mut arguments = arguments.into_iter();
-        let mut options = HashMap::new();
-        while let Some(flag) = arguments.next() {
-            let flag = flag
-                .into_string()
-                .map_err(|_| PreflightCommand::usage("option names must be UTF-8"))?;
-            if !flag.starts_with("--") {
-                return Err(PreflightCommand::usage(&format!(
-                    "expected an option, got {flag}"
-                )));
-            }
-            let value = arguments
-                .next()
-                .ok_or_else(|| PreflightCommand::usage(&format!("{flag} requires a value")))?;
-            if options.insert(flag.clone(), value).is_some() {
-                return Err(PreflightCommand::usage(&format!(
-                    "{flag} was provided more than once"
-                )));
-            }
-        }
-        Ok(options)
     }
 }
 
@@ -69,7 +43,7 @@ impl PreflightCommand {
             format!(
                 "{message}\n\
              usage:\n\
-             \x20 nook-preflight <preflight-command> [options]"
+             \x20 nook-preflight <preflight-command>"
             ),
         )
     }
